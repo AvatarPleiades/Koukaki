f81fbc0de1e12a414b0758ce048c97d731af71b5
AvatarPleiades <berry.julien57@gmail.com>
2023-07-05 08:52:21 +0200

    rotation des fleurs

---
+++ template-parts/oscars.php
@@ -0,0 +1,4 @@
+<div class="oscars">
+    <h2 class="titre_oscars">Fleurs d’oranger et chats errants est nominé aux Oscars Short Film Animated de 2022 !</h2>
+    <img src=<?php echo get_theme_file_uri("./assets/images/oscars.png");?> alt="logo des oscars">
+</div>
